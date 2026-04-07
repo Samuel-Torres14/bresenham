7c7fe141fdbcf5d309e0a0eac6dd6e290fbf8dd1
Samuel Torres <est.samuelj.torres@unimilitar.edu.co>
2026-04-06 23:14:11 -0500

    se leen los valores

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function dibujar() {
     console.log("dibujar...");
 }
+
+let x0 = document.getElementById("x0").value;
+let y0 = document.getElementById("y0").value;
+let x1 = document.getElementById("x1").value;
+let y1 = document.getElementById("y1").value;
