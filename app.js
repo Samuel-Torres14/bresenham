ef9261350bda68d8f7242d3ec7b4c4aca59670c5
Samuel Torres <est.samuelj.torres@unimilitar.edu.co>
2026-04-06 23:15:12 -0500

    se corrige un error para convertir en enteros

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function dibujar() {
     console.log("dibujar...");
 }
 
-let x0 = document.getElementById("x0").value;
-let y0 = document.getElementById("y0").value;
-let x1 = document.getElementById("x1").value;
-let y1 = document.getElementById("y1").value;
+let x0 = parseInt(document.getElementById("x0").value);
+let y0 = parseInt(document.getElementById("y0").value);
+let x1 = parseInt(document.getElementById("x1").value);
+let y1 = parseInt(document.getElementById("y1").value);
