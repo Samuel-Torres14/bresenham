64ff2c3745ddbee7b3fb9c983346d404449b34d4
Samuel Torres <est.samuelj.torres@unimilitar.edu.co>
2026-04-06 23:15:52 -0500

    se da el canvas y se limpia

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ let x0 = parseInt(document.getElementById("x0").value);
 let y0 = parseInt(document.getElementById("y0").value);
 let x1 = parseInt(document.getElementById("x1").value);
 let y1 = parseInt(document.getElementById("y1").value);
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
